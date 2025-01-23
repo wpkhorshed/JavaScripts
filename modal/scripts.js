05b3f14dd0f0c90614e7f21f1311158e0e45916a
khorshed alam <khorshedalamwp@gmail.com>
2025-01-23 15:58:19 +0600

    add modal features

--- modal/scripts.js
+++ modal/scripts.js
@@ -5,23 +5,28 @@ const closeModal = document.querySelector(".close-modal");
 const showModal = document.querySelectorAll(".show-modal");
 const overlay = document.querySelector(".overlay");
 
-// console.log(modal, closeModal, showModal);
 
 const modalOpen = function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  };
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-  
-const modalClose = function(){
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
+const modalClose = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 for (let i = 0; i < showModal.length; i++) {
-  showModal[i].addEventListener("click",modalOpen );
+  showModal[i].addEventListener("click", modalOpen);
 }
 
+closeModal.addEventListener("click", modalClose);
+overlay.addEventListener("click", modalClose);
 
-closeModal.addEventListener('click', modalClose);
-overlay.addEventListener('click',modalClose);
+document.addEventListener("keydown", function (e) {
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    modalClose();
+  }
+  
+});
